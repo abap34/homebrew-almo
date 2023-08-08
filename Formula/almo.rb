6f8dd01cfd1b3cd4717c3c5d069d3f0cacf4ef6d
abap34 <uchi_ymgc@her0.jp>
2023-08-08 23:25:18 +0900

    remove unnecessary command

--- Formula/almo.rb
+++ Formula/almo.rb
@@ -8,27 +8,12 @@ class Almo < Formula
     sha256 "eafb4e7f9cb745dd8496e840527e4449d9ab9aab3e6aa4b6d6f6cfe7b0eabaf3"
     license "MIT"
   
-    # depends_on "cmake" => :build
   
     def install
-      # ENV.deparallelize  # if your formula fails when building in parallel
-      # Remove unrecognized options if warned by configure
-      # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-      system "./configure", *std_configure_args, "--disable-silent-rules"
       bin.install "almo"
-      # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     end
   
     test do
-      # `test do` will create, run in and delete a temporary directory.
-      #
-      # This test will fail and we won't accept that! For Homebrew/homebrew-core
-      # this will need to be a test that verifies the functionality of the
-      # software. Run the test with `brew test almo`. Options passed
-      # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-      #
-      # The installed folder is not in the path, so use the entire path to any
-      # executables being tested: `system "#{bin}/program", "do", "something"`.
       system "false"
     end
   end
