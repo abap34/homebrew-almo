948273de9c15e8f6982523594ba60b472f010505
abap34 <uchi_ymgc@her0.jp>
2023-08-09 06:01:00 +0900

    update sha256

--- Formula/almo.rb
+++ Formula/almo.rb
@@ -1,21 +1,12 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Almo < Formula
     desc "Markdown transpiler which can be converted to HTML included with execution and judging environments"
     homepage ""
     url "https://github.com/abap34/ALMO/releases/download/v0.1.0-alpha/almo"
-    sha256 "eaaf5376c519d226305e458ef0da17cb210da5c75690fbc785e9a78b2345764c"
+    sha256 "b4072114eafa748c876f8b8f0600cc7d9a9e2ea274c267c255bab3ece8cd402f"
     license "MIT"
-  
   
     def install
       bin.install "almo"
     end
-  
-    test do
-      system "false"
-    end
-
 end
   
