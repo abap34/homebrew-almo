95f9704391a1412648bafa8109cd21cfeccf2177
abap34 <uchi_ymgc@her0.jp>
2023-08-08 23:45:51 +0900

    update sha256

--- Formula/almo.rb
+++ Formula/almo.rb
@@ -5,7 +5,7 @@ class Almo < Formula
     desc "Markdown transpiler which can be converted to HTML included with execution and judging environments"
     homepage ""
     url "https://github.com/abap34/ALMO/releases/download/v0.0.3-alpha/almo"
-    sha256 "eafb4e7f9cb745dd8496e840527e4449d9ab9aab3e6aa4b6d6f6cfe7b0eabaf3"
+    sha256 "eaaf5376c519d226305e458ef0da17cb210da5c75690fbc785e9a78b2345764c"
     license "MIT"
   
   
