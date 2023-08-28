903f602b79a6e1ac146047c3b0b8142d240574e4
abap34 <uchi_ymgc@her0.jp>
2023-08-29 00:32:53 +0900

    fix link

--- Formula/almo.rb
+++ Formula/almo.rb
@@ -1,7 +1,7 @@
 class Almo < Formula
     desc "Markdown transpiler which can be converted to HTML included with execution and judging environments"
     homepage ""
-    url "https://github.com/abap34/ALMO/releases/download/https://github.com/abap34/ALMO/releases/download/v0.2.1-alpha/almo"
+    url "https://github.com/abap34/ALMO/releases/download/v0.2.1-alpha/almo"
     sha256 "d840a79f247d3867ea6dd5324f28ca1bad8785a05e6cc8c1dbc3c72d0667cd05"
     license "MIT"
   
