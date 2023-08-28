5d1c260832ca5e918dd3a7b8e49f6989a76f93c9
abap34 <uchi_ymgc@her0.jp>
2023-08-29 00:36:48 +0900

    update hash for v.0.2.1-alpha

--- Formula/almo.rb
+++ Formula/almo.rb
@@ -2,7 +2,7 @@ class Almo < Formula
     desc "Markdown transpiler which can be converted to HTML included with execution and judging environments"
     homepage ""
     url "https://github.com/abap34/ALMO/releases/download/v0.2.1-alpha/almo"
-    sha256 "d840a79f247d3867ea6dd5324f28ca1bad8785a05e6cc8c1dbc3c72d0667cd05"
+    sha256 "909de0e8788d48c4a09dd16424285e732dea6d2a548e7f2b6ee54355e060fc86"
     license "MIT"
   
     def install
