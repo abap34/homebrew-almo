a628929631dee87c108fd8889f5f3f72bac3957c
abap34 <uchi_ymgc@her0.jp>
2024-01-02 01:24:30 +0900

    fix hash

--- Formula/almo.rb
+++ Formula/almo.rb
@@ -2,7 +2,7 @@ class Almo < Formula
     desc "Markdown transpiler which can be converted to HTML included with execution and judging environments"
     homepage ""
     url "https://github.com/abap34/ALMO/releases/download/v0.5.6-alpha/almo"
-    sha256 "ad100807d2f764efa303c8dc4ac59eef6723ebeb4938a83ab85fa9d6994cb9c7"
+    sha256 "2a404ee2f642e82810264188ebe36f65c3d5c1c1eb46410d45f64a7db101a393"
     license "MIT"
   
     def install
