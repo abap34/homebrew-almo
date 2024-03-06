af1d7085bfa78a57e0eb6ef7d6e1dd74969404e4
abap34 <uchi_ymgc@her0.jp>
2024-03-06 17:37:45 +0900

    v.0.7.0-alpha

--- Formula/almo.rb
+++ Formula/almo.rb
@@ -1,8 +1,8 @@
 class Almo < Formula
     desc "Markdown transpiler which can be converted to HTML included with execution and judging environments"
     homepage ""
-    url "https://github.com/abap34/ALMO/releases/download/v0.6.1-alpha/almo"
-    sha256 "600b3bf935fcf92e1c19dc2d14c1f046a92e43e3b4946f2eb8ec08307f43c1fd"
+    url "https://github.com/abap34/ALMO/releases/download/v0.7.0-alpha/almo"
+    sha256 "de018587bf3989fd00c9455481d51f6f7e84e4d11bceda8941ff37b3646c4134"
     license "MIT"
   
     def install
